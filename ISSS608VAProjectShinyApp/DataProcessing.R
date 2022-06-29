834e6c904c9f25ff6cacf1f1d226b548e8d5cb3f
wang0010t <wangtianqi1992@gmail.com>
2022-06-29 19:14:07 +0800

    Commit after deployment

--- ISSS608VAProjectShinyApp/DataProcessing.R
+++ ISSS608VAProjectShinyApp/DataProcessing.R
@@ -1,6 +1,6 @@
 #For processing large datasets by filtering out only necessary data and saving the filtered data into rds file
 
-checkin_journal <- read_csv("data/CheckinJournal.csv")
+checkin_journal <- read_csv("data/NOT UPLOADED/CheckinJournal.csv")
 checkin_journal$timestamp <- as.Date(checkin_journal$timestamp, "%Y-%m-%d")
 checkin_journal_selected <- checkin_journal%>%
   filter(`venueType` == c("Pub", "Restaurant", "Workplace"))
